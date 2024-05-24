5641e437f9eaa1141e20285953a13efcecdcff15
Jonathan Miroshnik <fistuk2009@gmail.com>
2024-05-24 13:50:14 +0300

    opened the data file succesfully

--- ex1.py
+++ ex1.py
@@ -1,18 +1,18 @@
+import zipfile
 import torch
 import numpy
 
 
 def main():
-    with open("ex1 data", 'rb') as dataFile:
-        for line in dataFile:
-            try:
-                # 'latin1'
-                print(torch.Tensor(numpy.frombuffer(line, dtype=numpy.int32)))
-                print(line.decode('utf-8'))
-            except UnicodeDecodeError:
-                print(line.decode('utf-8', errors='ignore'))
-
-
+    with zipfile.ZipFile("ex1 data", 'r') as zip_ref:
+        print("NEGATIVE VALUES")
+        with zip_ref.open("ex1 data/neg_A0201.txt", 'r') as neg:
+            for line in neg:
+                print(line.decode())
+        print("POSITIVE VALUES")
+        with zip_ref.open("ex1 data/pos_A0201.txt", 'r') as pos:
+            for line in pos:
+                print(line.decode())
 
 if __name__ == '__main__':
     main()
