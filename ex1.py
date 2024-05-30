2a671202ee91e825afa3487d1d57d2dba296f027
Jonathan Miroshnik <fistuk2009@gmail.com>
2024-05-30 14:05:24 +0300

    c and e missing

--- ex1.py
+++ ex1.py
@@ -96,7 +96,7 @@ class StringClassifier(torch.nn.Module):
     def forward(self, x):
         x = self.flatten(x)
         x = self.fc1(x)
-        # x = self.relu(x)
+        x = self.relu(x)
         x = self.fc2(x)
         return x
 
